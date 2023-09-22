5e3317bf93f70d993ad7ee6bf9caaa781e4a6479
user <khantkaneki@gmail.com>
2023-09-22 23:44:20 +0630

    Project Deploy Testing in Github

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        quicksand: ["Quicksand", "sans-serif"],
+      },
+      keyframes: {
+        wavey: {
+          "0%, 100%": {
+            transform: "scaleY(0.5)",
+          },
+          "50%": {
+            transform: "scaleY(1.5)",
+          },
+        },
+      },
+      animation: {
+        wavey: "wavey 1000ms linear infinite",
+      },
+    },
+  },
+  plugins: [require("./src/helper/tableCaption")],
+};
